453b58f07a5fbfd659abd883e85ef6e229d4d61e
v-cshuai <v-cshuai.gd@chinatelecom.cn>
2024-11-24 16:26:46 +0800

    update

--- 02_src_hello_react/App.js
+++ 02_src_hello_react/App.js
@@ -1,10 +1,12 @@
 // 创建容器组件
 import React, { Component } from "react";
+import Hello from "./components/Hello/Hello";
 
 export default class App extends Component {
   render() {
     return (
       <div>
+        <Hello />
       </div>
     );
   }
--- 02_src_hello_react/components/Hello/Hello.jsx
+++ 02_src_hello_react/components/Hello/Hello.jsx
@@ -1,12 +1,13 @@
 // 创建容器组件
 import React, { Component } from "react";
+import "./Hello.css";
 
-export default class App extends Component {
+export default class Hello extends Component {
   render() {
     return (
       <div>
+        <h1 className="title">Hello, React!</h1>
       </div>
     );
   }
 }
-
